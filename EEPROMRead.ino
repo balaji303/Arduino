89a154e0548fc2bc2a2e0e5d37ca48a5e3c67abf
Balaji <balajivelu303@outlook.com>
2019-10-15 19:32:22 +0530

    Update EEPROMRead.ino

--- EEPROMRead.ino
+++ EEPROMRead.ino
@@ -1,1 +1,20 @@
+#include "EEPROM.h"
+int a=511;
+int value;
 
+void fun(){ //function
+value=EEPROM.read(a);
+Serial.print(a);
+Serial.print(":");
+Serial.println(value);
+delay(300);
+}
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+for(;a>0;--a){
+  fun();
+}
+}
