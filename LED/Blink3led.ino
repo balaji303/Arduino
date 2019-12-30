f3d6b02b7cfcf872a654381c1068f785175b695e
Balaji <balajivelu303@outlook.com>
2019-12-30 10:42:35 +0530

    Update Blink3led.ino

--- LED/Blink3led.ino
+++ LED/Blink3led.ino
@@ -1,1 +1,19 @@
-
+void setup() {
+  pinMode(01, OUTPUT);
+  pinMode(02, OUTPUT);
+  pinMode(03, OUTPUT);
+}
+void loop() {
+  digitalWrite(04, HIGH);   
+  delay(1000);              
+  digitalWrite(04, LOW);    
+  delay(1500);  
+  digitalWrite(02, HIGH);   
+  delay(1000);              
+  digitalWrite(02, LOW);    
+  delay(1500);  
+  digitalWrite(03, HIGH);   
+  delay(1000);              
+  digitalWrite(03, LOW);    
+  delay(1500);  
+}
