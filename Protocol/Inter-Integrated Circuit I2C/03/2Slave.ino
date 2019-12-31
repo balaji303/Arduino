1ca111a8dc820d7f2cdf9b2808f6881c0fd76bf8
Balaji <balajivelu303@outlook.com>
2019-12-31 17:06:14 +0530

    Update 2Slave.ino

--- Protocol/Inter-Integrated Circuit I2C/03/2Slave.ino
+++ Protocol/Inter-Integrated Circuit I2C/03/2Slave.ino
@@ -1,1 +1,30 @@
+//03- Slave 2
+//address 10
+#include <Wire.h>
+void setup() {
+  // put your setup code here, to run once:
+Wire.begin(10);
+pinMode(13,OUTPUT);
+digitalWrite(13,LOW);
+Wire.onReceive(receiveEvent);
+}
 
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(100);
+}
+
+void receiveEvent(){
+  while(Wire.available()){
+      char s=Wire.read();
+      if(s=='H'){
+        digitalWrite(13,HIGH);
+      }
+      else if(s=='L'){
+        digitalWrite(13,LOW);
+      }
+      else{
+        //Do nothing
+      }
+  }
+}
