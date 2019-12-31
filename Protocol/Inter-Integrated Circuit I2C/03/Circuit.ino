2c0175fa763feadb135169bf0361f7512a16d91b
Balaji <balajivelu303@outlook.com>
2019-12-31 17:07:24 +0530

    Update Circuit.ino

--- Protocol/Inter-Integrated Circuit I2C/03/Circuit.ino
+++ Protocol/Inter-Integrated Circuit I2C/03/Circuit.ino
@@ -1,1 +1,7 @@
-
+/*
+Click This link below for Circuit Diagram...
+  
+  https://github.com/balaji303/Arduino/blob/master/Circuit%20Diagram/i2c.png
+Or 
+Check the Circuits Folder in Arduino Repository
+*/
