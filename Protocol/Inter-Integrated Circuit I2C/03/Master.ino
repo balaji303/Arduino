9143f3e0177cd11071a65bb3e2cf7421b058f726
Balaji <balajivelu303@outlook.com>
2019-12-31 17:06:37 +0530

    Update Master.ino

--- Protocol/Inter-Integrated Circuit I2C/03/Master.ino
+++ Protocol/Inter-Integrated Circuit I2C/03/Master.ino
@@ -1,1 +1,27 @@
+//Function Call
+#include <Wire.h>
+void high(int add)
+{
+  Wire.beginTransmission(add);
+  Wire.write('H');
+  Wire.endTransmission();
+delay(1000);
+}
+void low(int add)
+{
+  Wire.beginTransmission(add);
+  Wire.write('L');
+  Wire.endTransmission();
+delay(1000);
+}
+void setup() {
+Serial.begin(9600);
+Wire.begin();
+}
 
+void loop() {
+  high(10);
+  low(10);
+  high(5);
+  low(5);
+}
