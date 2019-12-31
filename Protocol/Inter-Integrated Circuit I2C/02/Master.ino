2e3c3a73b27d9172aa1379cb567885bb2545f464
Balaji <balajivelu303@outlook.com>
2019-12-31 17:02:31 +0530

    Update Master.ino

--- Protocol/Inter-Integrated Circuit I2C/02/Master.ino
+++ Protocol/Inter-Integrated Circuit I2C/02/Master.ino
@@ -1,1 +1,30 @@
+//02-MAster code
+#include <Wire.h>
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+ char a=5;
+// if(Serial.available()>0){
+//  a=Serial.read();
+// }
+ Wire.begin(10);
+ pinMode(13,OUTPUT);
+ digitalWrite(13,HIGH);
+ delay(1000);
+ digitalWrite(13,LOW);
+ Wire.requestFrom(10,a);   
+ while(Wire.available()){
+  char c=Wire.read();
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  Serial.println(c);
+  Serial.flush();
+  Wire.flush();
+ }
+}
 
+void loop() {
+
+delay(1000);
+}
