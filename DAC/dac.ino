e81b90a64bf62d3e2fc58c95e07fbe0e0e965877
Balaji <balajivelu303@outlook.com>
2020-05-18 07:33:21 +0530

    Update dac.ino

--- DAC/dac.ino
+++ DAC/dac.ino
@@ -1,1 +1,18 @@
+uint8_t fvolt; 
+void setup() {
+  // put your setup code here, to run once:
+pinMode(11,OUTPUT);
+}
 
+void loop() {
+  // put your main code here, to run repeatedly:
+fvolt = ((1.38/5.0)*255);
+/*
+1.38 = Required Voltage at the output pin
+5.00 = Max. readable voltage
+255  = Largest 8 bit value (uint8_t)
+
+After uploading the code check the Voltage at D11, we will get 1.38 
+*/
+analogWrite(11,fvolt);
+}
