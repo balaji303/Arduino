2324eea307f0a49ce125228c92ca924e14bc9a6a
balaji303 <balajivelu303@outlook.com>
2020-11-04 21:02:09 +0530

    PlatformIO

---
+++ PlatformIO/Projects/ArduinoBlink/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Balaji");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
