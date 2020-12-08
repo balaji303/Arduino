a73bc8488fb473860c8553f01023192f24973862
balaji303 <balajivelu303@outlook.com>
2020-12-08 22:05:31 +0530

    Create StructQueue.ino

---
+++ FreeRTOS/StructQueue/StructQueue.ino
@@ -0,0 +1,17 @@
+#include <Arduino_FreeRTOS.h>
+
+#include <queue.h>
+
+struct Arduino{
+  int pin;
+  int ReadValue;
+}
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
