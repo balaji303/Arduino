8ba0de4f566bc408c406bd87549669c5c18ac804
balaji303 <balajivelu303@outlook.com>
2020-12-10 22:12:22 +0530

    Update StructinQueue.ino

--- FreeRTOS/StructinQueue/StructinQueue.ino
+++ FreeRTOS/StructinQueue/StructinQueue.ino
@@ -6,9 +6,16 @@ struct Arduino{
   int pin;
   int ReadValue;
 }
+
+QueueHandle_t structQueue;
 void setup() {
   // put your setup code here, to run once:
-
+structQueue=xQueueCreate(10,sizeof(structQueue));
+if(structQueue!=NULL){
+  xTaskCreate(TaskLED,
+              "TaskLED",
+              );
+}
 }
 
 void loop() {
